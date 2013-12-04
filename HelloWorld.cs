8e6f5c69801d9864955f08be223f14ddd9d2eb0e
Henrik Almér <henrik.almer@gmail.com>
2013-12-04 11:57:50 +0100

    Made output more polite

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -2,6 +2,6 @@ public class HelloWorld
 {
    public static void Main()
    {
-      System.Console.WriteLine("Hello, World!");
+      System.Console.WriteLine("Hello, World! Nice to meet you.");
    }
 }

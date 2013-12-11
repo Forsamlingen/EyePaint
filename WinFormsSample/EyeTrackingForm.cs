3557a1442faf6524355153935f9d33f9d453a9e8
Carl Thomé <cthome@kth.se>
2013-12-11 17:53:37 +0100

    Altered SDK demo with eye painting
    Functionality not guaranteed, as I cannot test the program without an
    eye tracker.

--- WinFormsSample/EyeTrackingForm.cs
+++ WinFormsSample/EyeTrackingForm.cs
@@ -4,11 +4,26 @@
     using System.Drawing;
     using System.Windows.Forms;
     using Tobii.Gaze.Core;
+    using System.Collections.Generic;
+
+    struct Line
+    {
+        public Pen pen; //TODO Shouldn't be public.
+        public List<Point> points;
+
+        public Line(Pen pen)
+        {
+            this.pen = pen;
+            this.points = new List<Point>();
+        }        
+    }
 
     public partial class WinFormsSample : Form
     {
         private readonly EyeTrackingEngine _eyeTrackingEngine;
         private Point _gazePoint;
+        private List<Line> lines;
+        private bool draw;
         private delegate void UpdateStateDelegate(EyeTrackingStateChangedEventArgs eyeTrackingStateChangedEventArgs);
     
         public WinFormsSample(EyeTrackingEngine eyeTrackingEngine)
@@ -17,11 +32,32 @@
             Shown += OnShown;
             Paint += OnPaint;
             Move += OnMove;
+            KeyDown += OnKeyDown;
+            KeyUp += OnKeyUp;
             
             _eyeTrackingEngine = eyeTrackingEngine;
             _eyeTrackingEngine.StateChanged += StateChanged;
             _eyeTrackingEngine.GazePoint += GazePoint;
             _eyeTrackingEngine.Initialize();
+
+            lines = new List<Line>();
+            draw = false;
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs eventArgs)
+        {
+            if (eventArgs.KeyCode == Keys.Space)
+                draw = true;
+
+            // Prepare new line.
+            Pen pen = new System.Drawing.Pen(System.Drawing.Color.Black, 3); //TODO Let user select pen color, thickness, etc.
+            lines.Add(new Line(pen));
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs eventArgs)
+        {
+            if (eventArgs.KeyCode == Keys.Space)
+                draw = false;
         }
 
         private void OnMove(object sender, EventArgs eventArgs)
@@ -31,8 +67,28 @@
 
         private void OnPaint(object sender, PaintEventArgs paintEventArgs)
         {
+            //TODO Remove ugly hack.
+            int i = lines.Count - 1;
+            if (i < 0)
+                return;
+
+            // Get current eye position.
             var local = PointToClient(_gazePoint);
-            paintEventArgs.Graphics.FillEllipse(Brushes.Red, local.X - 25, local.Y - 25, 20, 20);
+
+            // Mark current eye position.
+            paintEventArgs.Graphics.FillEllipse(Brushes.Black, local.X - 25, local.Y - 25, 20, 20);
+
+            // Keep current eye position, but only if the user wants to.
+            Line currentLine = lines[lines.Count - 1];
+            if (draw)
+                currentLine.points.Add(local);
+
+            // Create drawing.
+            foreach (Line line in lines)
+            {
+                if (line.points.Count > 1)
+                    paintEventArgs.Graphics.DrawCurve(line.pen, line.points.ToArray());
+            }
         }
 
         private void GazePoint(object sender, GazePointEventArgs gazePointEventArgs)

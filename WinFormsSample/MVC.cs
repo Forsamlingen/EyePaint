ee9fa41cbd8d9b0d54a21363384ea17edbedb6cb
Carl Thomé <cthome@kth.se>
2013-12-18 18:02:17 +0100

    Various changes to interfaces

--- WinFormsSample/MVC.cs
+++ WinFormsSample/MVC.cs
@@ -2,28 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Drawing;
+using System.Diagnostics;
 
 namespace WinFormsSample
 {
     // Generates tree graphs.
     interface Model
     {
-        Point currentPoint;
-        List<Tree> components;
-        Tree generateTree(Point root);
+        Stack<Tree> components;
+        void createTree(Point root, Color color);
+        void expandTree();
     }
 
     // Rasterizes graphs into bitmaps.
     interface View
     {
-        Bitmap rasterizeTree(Tree tree);
+        Bitmap rasterizeTrees(IEnumerable<Tree> components);
     }
 
     // Handles user input. Main program.
     interface Controller
     {
+        Stopwatch stopwatch;
         //TODO Can the interface require the class constructor to register events?
         private void OnGreenButtonDown(object sender, EventArgs e);
+        private void OnGreenButtonUp(object sender, EventArgs e);
         private void OnRedButtonDown(object sender, EventArgs e);
+        private void OnRedButtonUp(object sender, EventArgs e);
     }
 }

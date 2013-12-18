752d705363a467bc54eff771d70f6c2f7ac692c1
Carl Thomé <cthome@kth.se>
2013-12-18 16:57:56 +0100

    Initial Model-View-Controller interfaces

---
+++ WinFormsSample/MVC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinFormsSample
+{
+    interface Model
+    {
+        // Grafer
+    }
+
+    interface View
+    {
+        // Givet graf skapa bitmap.
+    }
+
+    interface Controller
+    {
+        // Givet knapptryck, skriv ut bitmap, m.m. Huvudprogrammet.
+    }
+}
